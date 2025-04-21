d0cb5bf954eebdf518670b7d68ae3d65c20d250b
AlfSchool <alif.sheikh@itiszuccante.edu.it>
2025-04-21 21:26:27 +0200

    adjusted windows size

---
+++ graficTest/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Test gui = new Test();
+        gui.setSize(500, 4 00);
+        gui.setResizable(false);
+        gui.setLocationRelativeTo(null);
+        gui.setVisible(true);
+    }
+}
--- graficTest/src/Test.java
+++ graficTest/src/Test.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.List;
 
-public class Test {
+public class Test extends JFrame {
+
+    public Test() {
+        init();
+    }
+
+    private void init() {
+        setTitle("Test");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+    }
 }
