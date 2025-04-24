98b63c2268b646e3ff53bfc225d09e4100c17308
AlfSchool <alif.sheikh@itiszuccante.edu.it>
2025-04-24 15:25:21 +0200

    idk

--- graficTest/src/Main.java
+++ graficTest/src/Main.java
@@ -3,9 +3,6 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Main {
-    static int carIndex = 69;
-    static int width = 500;
-    static int height = 400;
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             TelemetryGUI gui = new TelemetryGUI(carIndex);
